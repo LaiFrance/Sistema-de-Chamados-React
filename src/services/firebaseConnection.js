c442d2c04330d56405d93df28534a208942a54c0
laifrance <laise94@hotmail.com>
2022-08-06 00:37:21 -0500

    initialize Firebase

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCPxK1x4AqNpMIiBW3FTvdd5tIXd_xzEj8",
+    authDomain: "sistema-309d8.firebaseapp.com",
+    projectId: "sistema-309d8",
+    storageBucket: "sistema-309d8.appspot.com",
+    messagingSenderId: "537225907929",
+    appId: "1:537225907929:web:cc6c53643dc92100dcb8c8",
+    measurementId: "G-10Q4Q0WZ78"
+  };
+  
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
