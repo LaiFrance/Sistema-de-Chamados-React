15889c8b3ae0b7a5b9daa3dea24d366416fb1dce
laifrance <laise94@hotmail.com>
2022-08-06 20:51:07 -0500

    add signin page and style

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,11 +1,40 @@
 
-function SingIn(){
-    return(
-      <div>
-        <h1>Página de Login</h1>
-      </div>
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './signin.css';
+import loginimg from '../../asserts/loginimg.png'
+
+function SignIn() {
+   /* meus estados */
   
-    );
+    
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('CLICOU')
   }
-  
-  export default SingIn;
+
+  return (
+    <div className="container-center">
+     <img className='sign-in' src={loginimg} alt="Logo" />
+      <div className="login">
+        <div className="login-area">
+        </div>
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input type="text" name="nome" value={nome} onChange={e => setNome(e.target.value)} placeholder="nome" />
+          <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.value) }/>
+          <input type="password" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value) } />
+          <button type="submit">Acessar</button>
+        </form>  
+
+        <Link to="/register">Criar uma conta</Link>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,3 +1,4 @@
+import './signup.css'
 
 function SingUp(){
     return(
